7e8d29b6e58a15b93bd7304752ca180d2f496573
Wichs03 <wicheyts@gmail.com>
2025-07-30 09:03:06 -0600

    Revert "Navbar"

--- src/components/profile/ProfileContent.jsx
+++ src/components/profile/ProfileContent.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import ProfileSidebar from "./ProfileSidebar";
-import Navbar from "../Layout/Navbar";
 
 export default function ProfileContent() {
   return (
-    <div>
-      <Navbar />
-      <div className="bg-gray-300 py-4 px-4 pt-20">
-        <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-4 md:gap-6">
-          <div className="w-full md:w-[300px]">
-            <ProfileSidebar />
-          </div>
+    <div className="bg-gray-300 py-8 px-4">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-4 md:gap-6">
+        <div className="w-full md:w-[300px]">
+          <ProfileSidebar />
+        </div>
 
-          <div className="flex-1 bg-white shadow-lg rounded-md h-auto border border-gray-200 mr-2">
-            <h2 className="text-xl font-semibold text-center text-gray-500 mt-10">
-              INFO EDITAR PERFIL
-            </h2>
-          </div>
+        <div className="flex-1 bg-white shadow-lg rounded-md h-auto border border-gray-200 mr-2">
+          <h2 className="text-xl font-semibold text-center text-gray-500 mt-10">
+            INFO EDITAR PERFIL
+          </h2>
         </div>
       </div>
     </div>
