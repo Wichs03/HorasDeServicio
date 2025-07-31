cc19567d5f929e86df48eb10a0ce7b5644997a43
Alzate1075 <edal1075@gmail.com>
2025-07-30 20:30:25 -0500

    Log out

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { logout } from "../../api/authService";
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-t from-gray-900 via-gray-600 to-gray-300 text-white px-6 py-10 flex">
-      <div className="max-w-7xl mx-auto grid md:grid-cols-3 gap-2">
+    <footer className="bg-gradient-to-t from-gray-900 via-gray-600 to-gray-300 text-white md:px-20 pt-10 pb-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {/* REDES SOCIALES */}
-        <div>
-          <h3 className="font-semibold mb-3">Contáctanos</h3>
-          <div className="flex mb-5 gap-4">
+        <div className="flex flex-col items-center md:justify-center md:items-start text-center md:text-left">
+          <h3 className="text-2xl md:text-lg font-semibold mb-3">
+            Contáctanos
+          </h3>
+          <div className="flex mb-4 gap-2">
             <a
               href="https://www.fundaciondevalores.org/"
               target="_blank"
@@ -17,10 +20,9 @@ export default function Footer() {
               <img
                 src="/Contactanos/WebFunval.png"
                 alt="WebFunval"
-                className="w-9 h-9"
+                className="w-10 h-10"
               />
             </a>
-
             <a
               href="https://www.estudiantefunval.org"
               target="_blank"
@@ -29,7 +31,7 @@ export default function Footer() {
               <img
                 src="/favicon A 32.ico"
                 alt="EstudianteFunval"
-                className="w-9 h-9"
+                className="w-10 h-10"
               />
             </a>
             <a
@@ -40,11 +42,11 @@ export default function Footer() {
               <img
                 src="/Contactanos/Email.png"
                 alt="E-Mail"
-                className="w-9 h-9"
+                className="w-10 h-10"
               />
             </a>
           </div>
-          <h3 className="font-semibold mb-3">Síganos</h3>
+          <h3 className="text-2xl md:text-lg font-semibold mb-3">Síganos</h3>
           <div className="flex gap-4">
             <a
               href="https://www.facebook.com/fundet?mibextid=LQQJ4d"
@@ -54,10 +56,9 @@ export default function Footer() {
               <img
                 src="/Social/Facebook.png"
                 alt="Facebook"
-                className="w-9 h-9"
+                className="w-8 h-8"
               />
             </a>
-
             <a
               href="https://www.youtube.com/channel/UC3mlp-KW6mSDrsfsp8OOlIQ"
               target="_blank"
@@ -66,10 +67,9 @@ export default function Footer() {
               <img
                 src="/Social/Youtube.png"
                 alt="Youtube"
-                className="w-9 h-9"
+                className="w-8 h-8"
               />
             </a>
-
             <a
               href="https://www.instagram.com/funvalinternacional?igshid=MzRlODBiNWFlZA%3D%3D"
               target="_blank"
@@ -78,50 +78,54 @@ export default function Footer() {
               <img
                 src="/Social/instagram.png"
                 alt="Instagram"
-                className="w-9 h-9"
+                className="w-8 h-8"
               />
             </a>
           </div>
         </div>
 
         {/* INFO */}
-        <div className="text-center">
-          <button className="bg-[#2899C4] px-4 py-2 rounded flex items-center justify-center gap-2 mx-auto mb-4">
-            <img src="/Soporte.png" alt="Soporte" className="w-6 h-6" />
+        <div className="text-center flex flex-col items-center justify-center mx-6 md:mx-0">
+          <button className="bg-[#2899C4] px-4 py-2 rounded flex items-center justify-center gap-2 mb-4 border shadow-xl">
+            <img
+              src="/Soporte.png"
+              alt="Soporte"
+              className="w-8 h-8 lg:w-6 lg:h-6"
+            />
             Contactar con el soporte del sitio
           </button>
-          <p>
-            Usted se ha identificado como{" "}
-            <strong>Edwin Dario Alzate López</strong>
+          <p className="mb-1">
+            Usted se ha identificado como <br />
+            <strong>Edwin Dario Álzate López</strong>
           </p>
-          <p className="text-sm text-gray-300 mb-2">(Cerrar sesión)</p>
-          <ul className="space-y-1 text-sm text-gray-400">
-            <li>Resumen de retención de datos</li>
-            <li>Descargar la app para dispositivos móviles</li>
-            <li>Reiniciar tour para usuario en esta página</li>
-          </ul>
+          <button
+            onClick={logout}
+            className="cursor-pointer border mt-2 px-3 py-2 bg-red-600 lg:bg-blue-700 lg:hover:bg-red-700 font-semibold text-white rounded-lg shadow transition-colors duration-300 transform hover:scale-105"
+          >
+            Cerrar Sesión
+          </button>
         </div>
 
         {/* STORE */}
-        <div className="text-right">
-          <h3 className="font-semibold mb-3">
+        <div className="text-center flex flex-col items-center justify-center lg:items-end mx-auto md:col-span-2 lg:col-span-1 mb-4">
+          <h3 className="text-center lg:text-end text-xl font-semibold mb-4 mx-6 lg:mx-0">
             Descargar la app para dispositivos móviles
           </h3>
           <div className="space-y-3">
             <img
               src="/store/app-store.svg"
-              alt="Google Play"
-              className="w-26 ml-auto"
+              alt="App Store"
+              className="w-32 lg:w-24"
             />
             <img
               src="/store/play-store.svg"
-              alt="App Store"
-              className="w-26 ml-auto"
+              alt="Google Play"
+              className="w-32 lg:w-24"
             />
             <img
               src="/store/windows-store.svg"
-              alt="App Store"
-              className="w-26 ml-auto"
+              alt="Windows Store"
+              className="w-32 lg:w-24"
             />
           </div>
         </div>
